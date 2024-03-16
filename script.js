271e160f793dbcaa20b4c72c1f9fe43b2b99ae20
tinanii <ni.tin@northeastern.edu>
2024-03-16 13:29:47 -0400

    update

---
+++ script.js
@@ -0,0 +1,73 @@
+const questions = [
+    {
+        question: "What is your favorite color?",
+        answers: [
+            { text: "Answer1", correct: false},
+            { text: "Answer2", correct: true},
+            { text: "Answer3", correct: false},
+            { text: "Answer4", correct: false},
+
+        ]
+    },
+    {
+        question: "What is your favorite color?",
+        answers: [
+            { text: "Answer1", correct: false},
+            { text: "Answer2", correct: true},
+            { text: "Answer3", correct: false},
+            { text: "Answer4", correct: false},
+
+        ]
+
+    },
+    {
+        question: "What is your favorite color?",
+        answers: [
+            { text: "Answer1", correct: false},
+            { text: "Answer2", correct: true},
+            { text: "Answer3", correct: false},
+            { text: "Answer4", correct: false},
+
+        ]
+
+    },
+    {
+        question: "What is your favorite color?",
+        answers: [
+            { text: "Answer1", correct: false},
+            { text: "Answer2", correct: true},
+            { text: "Answer3", correct: false},
+            { text: "Answer4", correct: false},
+
+        ]
+
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButton  = document.getElementById("answer-buttons");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    showQuestion();
+}
+
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn"); 
+        answerButton.appendChild(button);
+    
+    });
+}
+
+startQuiz();
